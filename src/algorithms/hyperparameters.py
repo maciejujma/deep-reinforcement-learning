19bb289446e21c293a8060d0e9723d55a132e6b4
maciejujma <maciejujma@gmail.com>
2023-01-04 22:04:36 +0100

    Added script with hardcoded algorithm's hyperparameters

---
+++ src/algorithms/hyperparameters.py
@@ -0,0 +1,8 @@
+cartpole_hyperparameters = {
+    "h_size": 16,
+    "n_training_episodes": 1000,
+    "n_evaluation_episodes": 10,
+    "max_t": 1000,
+    "gamma": 1.0,
+    "learning_rate": 1e-2
+}
