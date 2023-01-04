a3c26cd382083287a2c191080456eab5185906f0
maciejujma <maciejujma@gmail.com>
2023-01-04 22:05:26 +0100

    Added script with function evaluating policy based agents

---
+++ src/evalutation.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def agent_evaluation(evaluation_environment, max_steps, n_evaluation_episodes, policy):
+    """
+    Policy based agents evaluating function.
+
+    :param evaluation_environment:
+        Evaluation environment from gym library.
+    :param max_steps: int
+        Number of maximum steps in a single episode.
+    :param n_evaluation_episodes: int
+        Number of evaluation episodes.
+    :param policy: torch.nn.Module
+        Policy to evaluate.
+    :return: Tuple[float]
+        Mean and standard deviation of the all rewards.
+    """
+
+    all_episodes_rewards = []
+
+    for episode in range(n_evaluation_episodes):
+        episode_rewards = []
+        state = evaluation_environment.reset()
+
+        for step in range(max_steps):
+
+            action, _ = policy.act(state)
+            state, reward, done, _ = evaluation_environment.step(action)
+
+            if done:
+                break
+        all_episodes_rewards.append(sum(episode_rewards))
+
+    return np.mean(all_episodes_rewards), np.std(all_episodes_rewards)
