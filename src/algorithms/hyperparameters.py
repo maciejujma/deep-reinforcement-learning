ef45f4073771f10dd258924df93f9b21b0e174a9
maciejujma <maciejujma@gmail.com>
2023-01-07 16:56:16 +0100

    Added Q-learning hyperparameters

--- src/algorithms/hyperparameters.py
+++ src/algorithms/hyperparameters.py
@@ -6,3 +6,14 @@ cartpole_hyperparameters = {
     "gamma": 1.0,
     "learning_rate": 1e-2
 }
+
+q_learning_hyperparameters = {
+    "n_training_episodes": 10000,
+    "n_evaluation_episodes": 100,
+    "max_t": 99,
+    "gamma": 0.95,
+    "learning_rate": 0.7,
+    "decay_rate": 0.005,
+    "max_epsilon": 1.0,
+    "min_epsilon": 0.05
+}
