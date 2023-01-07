b5409b32d6272c4d849c1f0edfc77174aa607178
maciejujma <maciejujma@gmail.com>
2023-01-07 16:55:13 +0100

    Added q-learning agent evaluation

--- src/evalutation.py
+++ src/evalutation.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 import numpy as np
 
 
@@ -34,3 +36,28 @@ def agent_evaluation(evaluation_environment, max_steps, n_evaluation_episodes, p
         all_episodes_rewards.append(sum(episode_rewards))
 
     return np.mean(all_episodes_rewards), np.std(all_episodes_rewards)
+
+
+def evaluate_q_learning_agent(env, max_steps, n_eval_episodes, Q, seed):
+    episode_rewards = []
+    for episode in tqdm(range(n_eval_episodes)):
+        if seed:
+            state = env.reset(seed=seed[episode])
+        else:
+            state = env.reset()
+        total_rewards_ep = 0
+
+        for step in range(max_steps):
+            # Take the action (index) that have the maximum expected future reward given that state
+            action = np.argmax(Q[state][:])
+            new_state, reward, done, info = env.step(action)
+            total_rewards_ep += reward
+
+            if done:
+                break
+            state = new_state
+        episode_rewards.append(total_rewards_ep)
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+
+    return mean_reward, std_reward
